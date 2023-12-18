e9778832dbeae3ed8772f30c738d66685f8bd35f
Enzhy58 <ang.borisova2015@yandex.ru>
2023-12-18 15:24:49 +0300

    configure slick-slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(function () {
     $('.events__list').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-prev.svg" width="48" height="48" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-next.svg" width="48" height="48" alt=""></button>',
         slidesToShow: 3,
+        dots: false,
+        arrows: true,
     });
 });
