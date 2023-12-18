d8ed9d037167758e623f6ae46c18e7474175b324
Enzhy58 <ang.borisova2015@yandex.ru>
2023-12-18 16:57:35 +0300

    added choose section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('.events__list').slick({
+    $('.events__list, .choose__list').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-prev.svg" width="48" height="48" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-next.svg" width="48" height="48" alt=""></button>',
         slidesToShow: 3,
