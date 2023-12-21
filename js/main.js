aa800e4e6e8a66bc488a43b29d9166efed10d357
Enzhy58 <ang.borisova2015@yandex.ru>
2023-12-21 18:00:40 +0300

    connected the slick slider to the portfolio and correct the arrows

--- js/main.js
+++ js/main.js
@@ -6,4 +6,11 @@ $(function () {
         dots: false,
         arrows: true,
     });
+    $('.portfolio__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-prev.svg" width="48" height="48" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-next.svg" width="48" height="48" alt=""></button>',
+        slidesToShow: 1,
+        dots: false,
+        arrows: true,
+    });
 });
