da624cd44573c161032fbf9bc7d9bc0a850b11bb
Enzhy58 <ang.borisova2015@yandex.ru>
2023-12-22 18:26:02 +0300

    added adaptive events

--- js/main.js
+++ js/main.js
@@ -16,8 +16,30 @@ $(function () {
         $('.wrapper').removeClass('wrapper__fixed');
     });
 
+    $('.events__list').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-prev.svg" width="48" height="48" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-next.svg" width="48" height="48" alt=""></button>',
+        slidesToShow: 3,
+        dots: false,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 880,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+        ]
+    });
 
-    $('.events__list, .choose__list').slick({
+
+    $('.choose__list').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-prev.svg" width="48" height="48" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-next.svg" width="48" height="48" alt=""></button>',
         slidesToShow: 3,
